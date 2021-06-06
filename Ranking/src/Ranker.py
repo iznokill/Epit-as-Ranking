795d827113b5d192fa4255efd30c3f9bf4cdd0c5
youssef Kadaoui Abbassi <youssef;kadaoui-abbassi@epita.fr>
2021-06-06 02:08:59 +0200

    ERS: functionnal Json update

--- Ranking/src/Ranker.py
+++ Ranking/src/Ranker.py
@@ -1,13 +1,48 @@
 import json
 
 
+def game_to_iterable(game_file):
+    game_list = game_file.split("\n")
+    assert not len(game_list) % 3, "Given game stats are not complete"
+    return {game_list[i + 1]: game_list[i:i + 2] for i in range(0, len(game_list), 3)}
+
+
 def game_stats(game):
     game_file = open(game, 'r').read()
-    return game_file
+    return game_to_iterable(game_file)
+
+
+def update_json(json_object, game_object):
+    player_list = game_object.keys()
+    points = len(player_list)
+    players = json_object["players"]
+    ind = 0
+    for player in players:
+        if player["name"] in player_list:
+            points //= 2 if points > 1 else 1
+            json_object["players"][ind]["points"] += points
+        ind += 1
+
+    players = sorted(players, key=lambda k: k.get('points', 0), reverse=True)
+    rank = 0
+    for _ in players:
+        players[rank]["rank"] = rank + 1
+        rank += 1
+
+    json_object["players"] = players
+    return json_object
 
 
 def Ranker(file, game):
-    json_file = open(file, "r")
-    json_object = json.load(json_file)
-    json_file.close()
-    return str(json_object)
+    try:
+        json_file = open(file, "r")
+        game_object = game_stats(game)
+        json_object = json.load(json_file)
+        json_file.close()
+        json_object = update_json(json_object, game_object)
+        json_file = open(file, "w")
+        json.dump(json_object, json_file)
+        json_file.close()
+        return str(json_object)
+    except:
+        print("json file is not on point")
--- Ranking/tests/test.py
+++ Ranking/tests/test.py
@@ -1,10 +1,13 @@
 from Ranking.src.Ranker import Ranker
 
 
-def test_update(file, update_text, result):
+def test_update(file, update_text, expected):
     res = Ranker(file, update_text)
-    assert res == result, "Update failed\ngot:\n" + res + "\nexpected:\n" + result
+    assert res == expected, "Update failed\ngot:\n" + str(res) + "\nexpected:\n" + expected
 
 
 if __name__ == "__main__":
-    test_update("test_files/empty.json", "test_files/simple_game.txt", "{}")
+    test_update("test_files/empty.json", "test_files/simple_game.txt", None)
+    test_update("test_files/simple.json", "test_files/simple_game.txt",
+                "{'players': [{'name': 'turbo-ice', 'rank': 1, 'points': 10}, {'name': 'ScottPosey', 'rank': 2, "
+                "'points': 10}]}")
